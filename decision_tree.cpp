abe293884bbc593a6e0bfcabdf4c1df3a0e2c3f4
Valentin Denis <tintinsevres@gmail.com>
2023-10-24 17:48:41 +0200

    Modif
    Modifications de la fonction build

--- decision_tree.cpp
+++ decision_tree.cpp
@@ -11,12 +11,16 @@ using namespace std;
 //Fonction qui construit un arbre 
 DecisionTree::TreeNode* buildTree(const Dataset& data, const std::vector<std::string>& labels){
 
-    if(data.size() || labels.size()){ // Si l'une des valeurs est vide 
+    if(!data.size() || !labels.size()){ // Si l'une des valeurs est vide 
 
         return nullptr;
     }
 
     DecisionTree::TreeNode* node = new DecisionTree::TreeNode; // Création d'un noeud
+    
+    node ->Left = buildTree(data,labels);
+    node ->Right = buildTree(data,labels);
+
     return node;
 
 }
