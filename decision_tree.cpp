0f3fe4fbdde0ba0a2a45e1a9b8b5c9b78d03a4b6
Valentin Denis <tintinsevres@gmail.com>
2023-10-24 17:27:44 +0200

    AJout d'une fonction

---
+++ decision_tree.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "decision_tree.h"
+#include "data_loading.h"
+
+using namespace std;
+
+//Fonction qui construit un arbre 
+DecisionTree::TreeNode* buildTree(const Dataset& data, const std::vector<std::string>& labels){
+
+    if(data.size() || labels.size()){ // Si l'une des valeurs est vide 
+
+        return nullptr;
+    }
+
+    DecisionTree::TreeNode* node = new DecisionTree::TreeNode; // Création d'un noeud
+    return node;
+
+}
+
+
+
