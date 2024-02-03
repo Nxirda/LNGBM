ea0353021851185f55046016c9ec125950ba4750
Adrien Henrot <nxirda@Myrkr.home>
2024-02-03 21:13:23 +0100

    UPDATE [03/02]
    - Started fixing comments to generate doc with doxyfile in the include/Criteria directory

--- include/Criterias/Quartiles.hpp
+++ include/Criterias/Quartiles.hpp
@@ -4,24 +4,53 @@
 #include "ICriteria.hpp"
 #include <string>
 
+/** @class Quartiles
+ * @brief Class computing the quartiles of a given value distribution.
+ *
+ * This class implements the ICriteria interface.
+ */
 class Quartiles : public ICriteria {
 private:
   // Parameters
-  const std::string name = "Quartiles";
-  const std::vector<float> quartiles = {25.0, 50.0, 75.0};
+  const std::string name =
+      "Quartiles"; /**< A string variable to display the name. */
+  const std::vector<float> quartiles = {25.0, 50.0, 75.0}; /**< Array containing
+                                  the values in % of the quartiles*/
 
 public:
-  // Constructor
+  /**
+   * @brief Default constructor for the Quartiles class.
+   *
+   * Initializes the class with default parameters.
+   */
   Quartiles();
 
-  // Destructor
+  /**
+   * @brief Destructor for the Quartiles class.
+   */
   ~Quartiles() override;
 
-  // Methods
-  static std::string get_Name();
-
+  /**
+   * @brief Prints the name of the class on standard output.
+   */
   void print() override;
 
+  /**
+   * @brief Static method to get the name of the Quartiles criteria.
+   *
+   * @return The name of the Quartiles criteria.
+   */
+  static std::string get_Name();
+
+  /**
+   * @brief Computes quartiles values for a given data distribution.
+   *
+   * This method implements the computation of the values of the quartiles
+   * of the provided list of data and the bin parameter.
+   *
+   * @param vector<float> list: The distribution of data
+   * @return Type: vector<float>, A vector of the values of the quartiles
+   */
   std::vector<float> compute(const std::vector<float> list) const override;
 };
 
--- include/Criterias/RandomValues.hpp
+++ include/Criterias/RandomValues.hpp
@@ -4,31 +4,71 @@
 #include "ICriteria.hpp"
 #include <string>
 
+/** @class RandomValues
+ * @brief Class computing some random values in a given distribution of values.
+ *
+ * This class implements the ICriteria interface.
+ */
 class RandomValues : public ICriteria {
 private:
   // Parameters
-  const std::string name = "Random_Values";
-  int number_Of_Elements = 10;
+  const std::string name =
+      "Random_Values"; /**< A string variable to display the name. */
+  int number_Of_Elements =
+      10; /**< Integer representing the number of values to compute */
 
 public:
-  // Constructor
+  /**
+   * @brief Default constructor for the RandomValues class.
+   *
+   * Initializes the class with default parameters.
+   */
   RandomValues();
 
+  /**
+   * @brief Parameterized constructor for the RandomValues class.
+   *
+   * @param int x: The number of values to compute.
+   */
   RandomValues(int x);
 
-  // Destructor
+  /**
+   * @brief Destructor for the RandomValues class.
+   */
   ~RandomValues() override;
 
+  /**
+   * @brief Prints the name of the class on standard output.
+   */
+  void print() override;
+
+  /**
+   * @brief Static method to get the name of the RandomValues criteria.
+   *
+   * @return The name of the RandomValues criteria.
+   */
   static std::string get_Name();
 
-  // Methods
-  void print() override;
-    static void printf();
-
-
+  /**
+   * @brief Computes random values in given data distribution.
+   *
+   * This method implements the computation of number_Of_Elements of random
+   * values in the provided list of data.
+   *
+   * @param vector<float> list: The distribution of data
+   * @return Type: vector<float>, A vector of the random values
+   */
   std::vector<float> compute(const std::vector<float> list) const override;
 
-  float get_Random_Float(float min , float max) const;
+  /**
+   * @brief Computes a random float in given boundaries
+   * 
+   * @param float min: minimal value we can return
+   * @param float max: maximal value we can return
+   * 
+   * @return Type: float, A random flaot
+  */
+  float get_Random_Float(float min, float max) const;
 };
 
 #endif
