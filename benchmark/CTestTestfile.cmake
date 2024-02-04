1f3d6e8b3aedab08c85570fcadfb26e79d4478d1
Adrien Henrot <nxirda@Myrkr.home>
2024-02-04 22:56:57 +0100

    FILE FIX

--- benchmark/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nxirda/Desktop/PPN_Projet_MEM/benchmark
-# Build directory: /home/nxirda/Desktop/PPN_Projet_MEM/benchmark
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nxirda/Desktop/PPN_Projet_MEM/test
-# Build directory: /home/nxirda/Desktop/PPN_Projet_MEM/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
